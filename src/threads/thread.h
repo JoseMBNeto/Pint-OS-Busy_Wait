1762ed55bb059b82c2c0938c92d3350cc33ab8b9
joseMBNeto <miltoneto4231@gmail.com>
2026-09-21 15:53:00 -0300

    Adicionei uma chamada de função externa no .h e iniciação da função no .c

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -138,5 +138,6 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+extern struct list sleeping_list;
 
 #endif /* threads/thread.h */
